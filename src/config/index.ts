0fbf77f6cea32dc49f7c8eb6e0d41ac6f928833c
Horio@suiu <horiosuiu@users.noreply.github.com>
2024-03-13 19:33:03 +0900

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,3 @@
 export const SITE_NAME = 'suiu { code rain, system green. }' as string;
 
 export const SITE_URL = 'https://www.suiu.me' as string;
-
-export const SITE_IMG = 'assets/suiu-logo.svg' as string;
